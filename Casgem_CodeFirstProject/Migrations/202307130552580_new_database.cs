f83edbede40edcc68bafc39ddc0c8a25a5a7c837
sahin <sahin@DESKTOP-1SCT98J>
2023-07-13 09:00:17 +0300

    Add project files.

---
+++ Casgem_CodeFirstProject/Migrations/202307130552580_new_database.cs
@@ -0,0 +1,16 @@
+﻿namespace Casgem_CodeFirstProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class new_database : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
